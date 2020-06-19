c4938d280376f445f4d3b186ee15b306b8a25be6
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-19 11:43:49 -0700

    pizza cost function that returns total cost of pizza according to input

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,50 @@ function Pizza(name, size, crust, sauce, cheese, toppings) {
   this.toppings = toppings;
 }
 
+Pizza.prototype.cost = function() {
+  let cost = 0;
+  if (this.size === "Personal" || this.size === "Medium") {
+    cost += 3
+  } else if (this.size === "Large") {
+    cost += 5
+  } else if (this.size === "Party Size") {
+    cost += 6
+  }
+  if (this.crust === ("Thin Crust" || "Cauliflower")) {
+    cost += 2
+  } else if (this.crust === ("Deep Crust" || "Thick Crust" || "Cheese-filled")) {
+    cost += 3
+  }
+  if (this.sauce === "Marinara") {
+    cost += 1
+  } else {
+    cost += 2
+  }
+  if (this.cheese === ("Ricotta" || "Swiss")) {
+    cost += 2
+  } else {
+    cost += 1
+  }
+  cost += this.toppings.length;
+  return cost;
+}
+
 // UI logic
 
 $(document).ready(function() {
-  let name;
-  let size;
-  let crust;
-  let sauce;
-  let cheese;
-  let toppingsArray = [];
 
   $("#pizza-maker").submit(function() {
     event.preventDefault();
-    name = $("#name").val();
-    size = $("input:radio[name=size]:checked").val();
-    crust = $("#crust").val();
-    sauce = $("input:radio[name=sauce]:checked").val();
-    cheese = $("input:radio[name=cheese]:checked").val();
-    toppings = $("input:checkbox[name=toppings]:checked").each(function() {
+    const name = $("#name").val();
+    const size = $("input:radio[name=size]:checked").val();
+    const crust = $("#crust").val();
+    const sauce = $("input:radio[name=sauce]:checked").val();
+    const cheese = $("input:radio[name=cheese]:checked").val();
+    const toppings = $("input:checkbox[name=toppings]:checked").each(function() {
       const eachTopping = $(this).val()
-      toppingsArray.push(eachTopping);
     });
+    let pizzaInput = new Pizza(name, size, crust, sauce, cheese, toppings);
+    $("#pizza-cost").text("Total cost: " + pizzaInput.cost())
+    console.log(pizzaInput);
   });
-
-  let pizza = new Pizza(name, size, crust, sauce, cheese, toppingsArray)
 });
