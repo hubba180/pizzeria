12826c836b33bb7031b0dbfaaf81b8f52e85d71b
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-19 09:43:16 -0700

    define pizza object

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic for Pizza Object
+
+function Pizza(name, size, crust, sauce, cheese) {
+  this.name = name
+  this.size = size
+  this.crust = crust
+  this.sauce = sauce
+  this.cheese = cheese
+  this.toppings = [];
+}
