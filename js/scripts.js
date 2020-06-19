be21287d63dc13010559100fb3ab5c1ced25d584
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-19 13:39:29 -0700

    add remove button inside cart

--- js/scripts.js
+++ js/scripts.js
@@ -53,10 +53,29 @@ Pizza.prototype.cost = function() {
 
 // UI logic
 
+function attachEventListeners(vialeArcabeleno, index) {
+  $("#cart").on("click", "button", function() {
+    pizzaItem = parseInt($(this).attr("id"))
+    delete vialeArcabeleno.pizzas[pizzaItem - 1]
+    let newArray = [];
+    vialeArcabeleno.pizzas.forEach(function(element) {
+      if (element) {
+        newArray.push(element)
+      }
+    });
+    vialeArcabeleno.pizzas = newArray;
+    console.log(vialeArcabeleno.pizzas)
+    $("#cart").empty()
+    vialeArcabeleno.pizzas.forEach(function(element) {
+      $("#cart").append(`<div id='pizza${index}'>1 ${element.size} pizza for ${element.name} <br> Crust: ${element.crust}<br> Sauce: ${element.sauce}<br> Cheese: ${element.cheese}<br> Toppings: ${element.toppingsArray}<br><button id='${vialeArcabeleno.pizzas.length}'>Remove Pizza</button></div>`)
+    })
+  })
+}
+
 $(document).ready(function() {
   let vialeArcabeleno = new Pizzeria()
-  let index = 0
-
+  let index = 1;
+  attachEventListeners(vialeArcabeleno, index);
   $("#pizza-maker").submit(function() {
     event.preventDefault();
     const name = $("#name").val();
@@ -64,16 +83,18 @@ $(document).ready(function() {
     const crust = $("#crust").val();
     const sauce = $("input:radio[name=sauce]:checked").val();
     const cheese = $("input:radio[name=cheese]:checked").val();
+    let toppingsArray = [];
     const toppings = $("input:checkbox[name=toppings]:checked").each(function() {
       const eachTopping = $(this).val()
+      toppingsArray.push(eachTopping);
     });
-    let pizzaInput = new Pizza(name, size, crust, sauce, cheese, toppings);
+    let pizzaInput = new Pizza(name, size, crust, sauce, cheese, toppingsArray);
     $("#pizza-cost").text("Total cost: " + pizzaInput.cost())
     vialeArcabeleno.pizzas.push(pizzaInput);
-    index++
-    $("#cart").append(`1 ${pizzaInput.size} pizza for ${pizzaInput.name} <br> Crust: ${pizzaInput.crust}<br> Sauce: ${pizzaInput.sauce}<br> Cheese: ${pizzaInput.cheese}<br> Toppings: ${pizzaInput.toppings}<br>`)
+    $("#cart").append(`<div id='pizza${index}'>1 ${pizzaInput.size} pizza for ${pizzaInput.name} <br> Crust: ${pizzaInput.crust}<br> Sauce: ${pizzaInput.sauce}<br> Cheese: ${pizzaInput.cheese}<br> Toppings: ${pizzaInput.toppingsArray}<br><button id='${vialeArcabeleno.pizzas.length}'>Remove Pizza</button></div>`)
     $("#order-cost").text(`Your total cost: $${vialeArcabeleno.totalCost()}`);
     $("#total-pizzas").text(`Pizzas in cart: ${vialeArcabeleno.pizzas.length}`);
     console.log(vialeArcabeleno.pizzas)
+    index++
   });
 });
