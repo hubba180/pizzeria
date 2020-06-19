a990e76582eb650aa750b2d9f2c037d288cf18d7
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-19 10:46:57 -0700

    answers are saved into pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 // Business Logic for Pizza Object
 
-function Pizza(name, size, crust, sauce, cheese) {
+function Pizza(name, size, crust, sauce, cheese, toppings) {
   this.name = name
   this.size = size
   this.crust = crust
   this.sauce = sauce
   this.cheese = cheese
-  this.toppings = [];
+  this.toppings = toppings;
 }
+
+// UI logic
+
+$(document).ready(function() {
+  let name;
+  let size;
+  let crust;
+  let sauce;
+  let cheese;
+  let toppingsArray = [];
+
+  $("#pizza-maker").submit(function() {
+    event.preventDefault();
+    name = $("#name").val();
+    size = $("input:radio[name=size]:checked").val();
+    crust = $("#crust").val();
+    sauce = $("input:radio[name=sauce]:checked").val();
+    cheese = $("input:radio[name=cheese]:checked").val();
+    toppings = $("input:checkbox[name=toppings]:checked").each(function() {
+      const eachTopping = $(this).val()
+      toppingsArray.push(eachTopping);
+    });
+  });
+
+  let pizza = new Pizza(name, size, crust, sauce, cheese, toppingsArray)
+});
